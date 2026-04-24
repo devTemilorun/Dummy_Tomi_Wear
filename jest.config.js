d8b3b7c0bf5f24420452725451c68516c7b54f79
Temilorun Olawuni <dev.temilorun@gmail.com>
2026-04-24 09:41:39 +0100

    jest configuration for next.js

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+/** @type {import('jest').Config} */
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
+  collectCoverageFrom: [
+    'app/**/*.{ts,tsx}',
+    'components/**/*.{ts,tsx}',
+    'hooks/**/*.{ts,tsx}',
+    'lib/**/*.{ts,tsx}',
+    '!**/*.d.ts',
+  ],
+}
+
+module.exports = createJestConfig(customJestConfig)
